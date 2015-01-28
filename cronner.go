0e257b3e703757ac3df3a1461085bed4d9b1dcde
Tim Heckman <tim@pagerduty.com>
2015-01-27 17:47:33 -0800

    initial commit of cronner

---
+++ cronner.go
@@ -0,0 +1,204 @@
+// Copyright 2014 PagerDuty, Inc.
+// All rights reserved - Do not redistribute!
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/PagerDuty/godspeed"
+	"github.com/codeskyblue/go-uuid"
+	"github.com/jessevdk/go-flags"
+)
+
+// MaxBody is the maximum length of a event body
+const MaxBody = 1536
+
+// args is for argument parsing
+type args struct {
+	Label  string `short:"l" long:"label" default:"" description:"name for cron job, cronner will lowercase it"`
+	Cmd    string `short:"c" long:"command" default:"" description:"command to run (please use full path) and its args"`
+	Events bool   `short:"e" long:"event" default:"false" description:"emit a start and end datadog event"`
+}
+
+// parse function configures the go-flags parser and runs it
+// it also does some light input validation
+func (a *args) parse() error {
+	p := flags.NewParser(a, flags.HelpFlag|flags.PassDoubleDash)
+
+	_, err := p.Parse()
+
+	// determine if there was a parsing error
+	// unfortunately, help message is returned as an error
+	if err != nil {
+		if !strings.Contains(err.Error(), "Usage") {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(-1)
+		} else {
+			fmt.Printf("%v\n", err.Error())
+			os.Exit(0)
+		}
+	}
+
+	r := regexp.MustCompile("^[a-zA-Z0-9_\\.]+$")
+
+	if !r.MatchString(a.Label) {
+		return fmt.Errorf("cron label '%v' is invalid, it can only be alphanumeric with underscores and periods", a.Label)
+	}
+
+	if len(a.Cmd) == 0 {
+		return fmt.Errorf("you must specify a command to run")
+	}
+
+	// lowercase the metric to try and encourage sanity
+	a.Label = strings.ToLower(a.Label)
+
+	return nil
+}
+
+func runCommand(cmd *exec.Cmd, label string, gs *godspeed.Godspeed) ([]byte, float64, error) {
+	var b bytes.Buffer
+
+	// comnbine stdout and stderr to the same buffer
+	cmd.Stdout = &b
+	cmd.Stderr = &b
+
+	// log start time
+	s := time.Now().UTC()
+
+	err := cmd.Run()
+
+	// This next section computes the wallclock run time in ms.
+	// However, there is the unfortunate limitation in that
+	// it uses the clock that gets adjusted by ntpd. Within pure
+	// Go, I don't have access to CLOCK_MONOTONIC_RAW.
+	//
+	// However, based on our usage I don't think we care about it
+	// being off by a few milliseconds.
+	t := time.Since(s).Seconds() * 1000
+
+	// emit the metric for how long it took us
+	gs.Timing(fmt.Sprintf("cron.%v.time", label), t, nil)
+
+	return b.Bytes(), t, err
+}
+
+// emit a godspeed (dogstatsd) event
+func emitEvent(title, body, alertType, uuidStr string, g *godspeed.Godspeed) {
+	var buf bytes.Buffer
+
+	// if the event's body is bigger than MaxBody
+	if len(body) > MaxBody {
+		// push the first MaxBody/2 bytes in to the buffer
+		buf.WriteString(body[0 : MaxBody/2])
+
+		// add indication of truncated output to the buffer
+		buf.WriteString("...\n=== OUTPUT TRUNCATED ===\n")
+
+		// add the last 1024 bytes to the buffer
+		buf.WriteString(body[len(body)-((MaxBody/2)+1) : len(body)-1])
+
+		body = string(buf.Bytes())
+	}
+
+	fields := make(map[string]string)
+	fields["source_type_name"] = "cron"
+
+	if len(alertType) > 0 {
+		fields["alert_type"] = alertType
+	}
+
+	if len(uuidStr) > 0 {
+		fields["aggregation_key"] = uuidStr
+	}
+
+	g.Event(title, body, fields, nil)
+}
+
+func main() {
+	// get and parse the command line options
+	opts := &args{}
+	err := opts.parse()
+
+	// make sure parsing didn't bomb
+	if err != nil {
+		// print the error to stderr and exit -1
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(-1)
+	}
+
+	// build a Godspeed client
+	gs, err := godspeed.NewDefault()
+
+	// make sure nothing went wrong with Godspeed
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(-1)
+	}
+
+	// get the hostname and validate nothing happened
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(-1)
+	}
+
+	// split the command in to its binary and arguments
+	cmdParts := strings.Split(opts.Cmd, " ")
+
+	// build the args slice
+	var args []string
+	if len(cmdParts) > 1 {
+		args = cmdParts[1:]
+	}
+
+	// get the *exec.Cmd instance
+	cmd := exec.Command(cmdParts[0], args...)
+
+	var uuidStr string
+
+	if opts.Events {
+		uuidStr = uuid.New()
+		// emit a DD event to indicate we are starting the job
+		emitEvent(fmt.Sprintf("Cron %v starting on %v", opts.Label, hostname), "job starting", "info", uuidStr, gs)
+	}
+
+	// run the command and return the output as well as the return status
+	out, wallRtMs, err := runCommand(cmd, opts.Label, gs)
+
+	// default variables are for success
+	// we change them later if there was a failure
+	var ret int
+	msg := "succeeded"
+	alertType := "success"
+
+	// if the command failed we want the exit status code
+	// and to change the state variables to their failure values
+	if err != nil {
+		msg = "failed"
+		alertType = "error"
+
+		if ee, ok := err.(*exec.ExitError); ok {
+			status := ee.Sys().(syscall.WaitStatus)
+			ret = status.ExitStatus()
+		} else {
+			ret = -1
+		}
+	}
+
+	// build the pieces of the completion event
+	title := fmt.Sprintf("Cron %v %v in %.5f seconds on %v", opts.Label, msg, wallRtMs/1000, hostname)
+	body := fmt.Sprintf("exit code: %d\noutput:\n%v", ret, string(out))
+
+	if opts.Events {
+		emitEvent(title, body, alertType, uuidStr, gs)
+	}
+}
---
+++ cronner_test.go
@@ -0,0 +1,222 @@
+// Copyright 2014 PagerDuty, Inc.
+// All rights reserved - Do not redistribute!
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"net"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/PagerDuty/godspeed"
+	"github.com/codeskyblue/go-uuid"
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct {
+	gs   *godspeed.Godspeed
+	l    *net.UDPConn
+	ctrl chan int
+	out  chan []byte
+}
+
+var _ = Suite(&TestSuite{})
+
+func (t *TestSuite) SetUpSuite(c *C) {
+	var err error
+	t.gs, err = godspeed.NewDefault()
+	c.Assert(err, IsNil)
+}
+
+func (t *TestSuite) TearDownSuite(c *C) {
+	t.gs.Conn.Close()
+}
+
+func (t *TestSuite) SetUpTest(c *C) {
+	t.l, t.ctrl, t.out = buildListener(8125)
+
+	// this goroutine will get cleaned up by the
+	// TearDownTest function
+	go listener(t.l, t.ctrl, t.out)
+}
+
+func (t *TestSuite) TearDownTest(c *C) {
+	close(t.ctrl)
+	t.l.Close()
+}
+
+func (t *TestSuite) Test_runCommand(c *C) {
+	//
+	// Test a command that finishes in 0.3 seconds
+	//
+	cmd := exec.Command("/usr/bin/time", "-p", "/bin/sleep", "0.3")
+
+	r, time, err := runCommand(cmd, "testCmd", t.gs)
+	c.Assert(err, IsNil)
+
+	stat, ok := <-t.out
+	c.Assert(ok, Equals, true)
+
+	statRegex := regexp.MustCompile("^cron.testCmd.time:([0-9\\.]+)\\|ms$")
+	match := statRegex.FindAllStringSubmatch(string(stat), -1)
+	c.Assert(len(match), Equals, 1)
+	c.Assert(len(match[0]), Equals, 2)
+
+	statFloat, err := strconv.ParseFloat(match[0][1], 64)
+	c.Assert(err, IsNil)
+	c.Assert(statFloat, Equals, time)
+
+	var timely bool
+
+	// assume the command run time will be within 20ms of correct,
+	// note sure how tight we can make this window without incurring
+	// false-failures.
+	if time > 300 && time < 320 {
+		timely = true
+	}
+	c.Assert(timely, Equals, true)
+
+	timeRegex := regexp.MustCompile("((?m)^real[[:space:]]+([0-9\\.]+)$)")
+	match = timeRegex.FindAllStringSubmatch(string(r), -1)
+	c.Assert(len(match), Equals, 1)
+	c.Assert(len(match[0]), Equals, 3)
+	c.Assert(match[0][2], Equals, "0.30")
+
+	//
+	// Test a command that finishes in 1 second
+	//
+
+	// Reset variables used
+	r = nil
+	err = nil
+	cmd = nil
+	time = 0
+	timeRegex = nil
+	match = nil
+	timely = false
+
+	cmd = exec.Command("/usr/bin/time", "-p", "/bin/sleep", "1")
+
+	r, time, err = runCommand(cmd, "testCmd", t.gs)
+	c.Assert(err, IsNil)
+
+	stat, ok = <-t.out
+	c.Assert(ok, Equals, true)
+
+	match = statRegex.FindAllStringSubmatch(string(stat), -1)
+	c.Assert(len(match), Equals, 1)
+	c.Assert(len(match[0]), Equals, 2)
+
+	statFloat, err = strconv.ParseFloat(match[0][1], 64)
+	c.Assert(err, IsNil)
+	c.Assert(statFloat, Equals, time)
+
+	if time > 1000 && time < 1020 {
+		timely = true
+	}
+	c.Assert(timely, Equals, true)
+
+	timeRegex = regexp.MustCompile("((?m)^real[[:space:]]+([0-9\\.]+)$)")
+	match = timeRegex.FindAllStringSubmatch(string(r), -1)
+
+	c.Assert(len(match), Equals, 1)
+	c.Assert(len(match[0]), Equals, 3)
+	c.Assert(match[0][2], Equals, "1.00")
+}
+
+func (t *TestSuite) Test_emitEvent(c *C) {
+	title := "TE"
+	body := "B"
+	alertType := "info"
+	uuidStr := uuid.New()
+
+	emitEvent(title, body, alertType, uuidStr, t.gs)
+
+	event, ok := <-t.out
+	c.Assert(ok, Equals, true)
+
+	eventStub := fmt.Sprintf("_e{%d,%d}:%v|%v|k:%v|s:cron|t:%v", len(title), len(body), title, body, uuidStr, alertType)
+	eventStr := string(event)
+
+	c.Assert(eventStr, Equals, eventStub)
+
+	//
+	// Test truncation
+	//
+
+	// generate a body that will be truncated
+	body = randString(2100)
+	title = "TE2"
+	alertType = "success"
+
+	emitEvent(title, body, alertType, uuidStr, t.gs)
+
+	event, ok = <-t.out
+	c.Assert(ok, Equals, true)
+
+	// simulate truncation and addition of the truncation messsage
+	truncatedBody := fmt.Sprintf("%v...\\\\n=== OUTPUT TRUNCATED ===\\\\n%v", body[0:MaxBody/2], body[len(body)-((MaxBody/2)+1):len(body)-1])
+
+	eventStub = fmt.Sprintf("_e{%d,%d}:%v|%v|k:%v|s:cron|t:%v", len(title), len(truncatedBody), title, truncatedBody, uuidStr, alertType)
+	eventStr = string(event)
+
+	c.Assert(eventStr, Equals, eventStub)
+}
+
+//
+// Cronner testing helper functions
+//
+var chars = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
+func randString(size int) string {
+	buf := make([]byte, size)
+	for i := range buf {
+		buf[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(buf)
+}
+
+func listener(l *net.UDPConn, ctrl <-chan int, c chan<- []byte) {
+	for {
+		select {
+		case _, ok := <-ctrl:
+			if !ok {
+				close(c)
+				return
+			}
+		default:
+			buffer := make([]byte, 8193)
+
+			_, err := l.Read(buffer)
+
+			if err != nil {
+				continue
+			}
+
+			c <- bytes.Trim(buffer, "\x00")
+		}
+	}
+}
+
+func buildListener(port uint16) (*net.UDPConn, chan int, chan []byte) {
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("127.0.0.1:%d", port))
+
+	if err != nil {
+		panic(fmt.Sprintf("getting address for test listener failed, bailing out. Here's everything I know: %v", err))
+	}
+
+	l, err := net.ListenUDP("udp", addr)
+
+	if err != nil {
+		panic(fmt.Sprintf("unable to listen for traffic: %v", err))
+	}
+
+	return l, make(chan int), make(chan []byte)
+}
