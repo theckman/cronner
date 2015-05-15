b4be5261f146f0459fc59f382ef5da94b73ee438
Tim Heckman <tim@pagerduty.com>
2015-05-15 10:56:47 -0700

    release cronner as open source under BSD-3 Clause license
    This adds a BSD 3-Clause license file, as well as updates the license header on all source files to indicate the source code is copyrighted to PagerDuty, Inc. and its usage is goverened by the BSD 3-Clause license. In addition, a contributors list has been added to the `README.md` file.
    
    In addition, the Development instructions in the `README.md` file have been modified to remove the Godep/gpm suggestion as none of the build steps actually use `gpm` or `Godep` to ensure the right versions of the dependencies are being used. No use suggesting it if we don't actually use it.

--- args.go
+++ args.go
@@ -1,5 +1,6 @@
-// Copyright 2014-2015 PagerDuty, Inc.
-// All rights reserved - Do not redistribute!
+// Copyright 2015 PagerDuty, Inc, et al. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license that can be found in the LICENSE file.
 
 package main
 
